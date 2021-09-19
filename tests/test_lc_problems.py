45c78570e99fff8575bc6f19e3a2644e8db946d3
Nikhil Ugale <nikhil10.ugale@gmail.com>
2021-09-19 15:46:08 +0530

    added

--- tests/test_lc_problems.py
+++ tests/test_lc_problems.py
@@ -1,7 +1,10 @@
 from src.implementstr import strStr
-
+from src.add import addTwoNumbers
 def test_strStr():
     assert strStr('hello','ll')==2
 
 def test1_strStr():
     assert strStr('hello','o')==4
+
+def test_addTwoNumbers():
+    assert addTwoNumbers(23,5)==28
